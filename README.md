3065d39a3a547f020fc54471bdde582171067b05
Huseyin BABAL <huseyinbabal88@gmail.com>
2018-05-31 10:45:40 +0300

    HashMap resize is used to prove non-thread safety

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 ### How to Run
 
 `mvn clean test`
+
+### Description
+In this test, I tried to use HashMap's resize operation to show you that HashMap is not thread-safe.
+As in the [HashMap Javadoc](http://hg.openjdk.java.net/jdk8/jdk8/jdk/file/687fd7c7986d/src/share/classes/java/util/HashMap.java#l56), HashMap tries to
+resize itself and build hash again when it is about to reach capacity by using some threshold.
+During resize operation, there will be clean list and old items will be copied there. During this operation
+there may be some null values if you try to do `HashMap#get`, since it is not ready yet.
+
+In codebase, I simply put a value in hashmap in an index more than `1000`. On one thread, I tried to set values to hashmap (to trigger resize on HashMap),
+and on main thread I tried to read value on certain index to check I have a null value due to resize operation.
--- src/test/java/com/huseyin/HashMapThreadSafetyTest.java
+++ src/test/java/com/huseyin/HashMapThreadSafetyTest.java
@@ -1,59 +1,38 @@
 package com.huseyin;
 
-import static org.junit.Assert.assertNotEquals;
-
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
-/**
- * This is a simulation of "HashMap is not thread safe".
- * There is so called user database with HashMap and
- * trying to add users concurrently. At a point, same ids
- * are inserted at the same time that causes key overwriting.
- * This results in threadCount != userId size.
- */
 public class HashMapThreadSafetyTest {
 
-    /**
-     * So called user database
-     */
-    class Users {
-        final Map<Integer, String> users = new HashMap<>();
-        int add(String username) {
-            final int nextAvailableId = this.users.size() + 1;
-            this.users.put(nextAvailableId, username);
-            return nextAvailableId;
-        }
-    }
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
-    @Test
+    @Test(timeout = 10000)
     public void shouldNotBeThreadSafeWhenHashMapProvided() {
-        int threadCount = 1000;
-        Users users = new Users();
-        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-        List<Future<Integer>> futures = new ArrayList<>(threadCount);
-        for (int i = 0; i < threadCount; i++) {
-            int userId = i;
-            futures.add(executorService.submit(() -> users.add(String.format("User-%s", userId))));
-        }
+        expectedEx.expect(RuntimeException.class);
+        expectedEx.expectMessage("Reference item is missing.");
 
-        Set<Integer> userIds = futures.stream().map(future -> {
-            try {
-                return future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                throw new RuntimeException();
+        final Map<Integer, String> map = new HashMap<>();
+
+        final int refKey = 10000;
+        final String refValue = "value1";
+        map.put(refKey, refValue);
+
+        new Thread(() -> IntStream.range(0, refKey).forEach(key -> map.put(key, "value2"))).start();
+
+        int count = 1;
+        while (true) {
+            String currentValue = map.get(refKey);
+            if (!refValue.equals(currentValue)) {
+                System.out.println(String.format("Iteration: %s, Ref Value: %s, Updated Value: %s", count, refValue, currentValue));
+                throw new RuntimeException("Reference item is missing.");
             }
-        }).collect(Collectors.toSet());
-
-        assertNotEquals(threadCount, userIds.size());
+            count++;
+        }
     }
 }
